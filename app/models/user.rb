7bd229773467920fefceae900eb949f312bc4583
HCDevid <hcdevid@gmail.com>
2014-04-15 12:13:20 -0400

    Working Users model, need to finish controller config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140415140759) do
+ActiveRecord::Schema.define(version: 20140415142136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20140415140759) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "avatar"
-    t.string   "password"
+    t.string   "password_digest"
   end
 
 end
