3174e5f59fda8ec5b619b7516de74574b7a85b71
HCDevid <hcdevid@gmail.com>
2014-04-15 12:31:53 -0400

    finished user signup, user show and edit still not done yet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,21 @@ class UsersController < ApplicationController
 
   def index
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :username, :password, :password_confirmation, :avatar)
+    end
 end
+
+
+
